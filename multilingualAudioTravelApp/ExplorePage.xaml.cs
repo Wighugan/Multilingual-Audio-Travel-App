9d809d5c873fd8eb1ed71f9e70acf7df15a9b56c
khang <khangnguyenhhhh1412@gmail.com>
2026-01-26 22:09:46 +0700

    icon + explore page

---
+++ multilingualAudioTravelApp/ExplorePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace multilingualAudioTravelApp;
+
+public partial class ExplorePage : ContentPage
+{
+    public ExplorePage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSearchClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Search", "Tìm kiếm", "OK");
+    }
+
+    private void OnNotifyClicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Thông báo", "Chưa có thông báo", "OK");
+    }
+}

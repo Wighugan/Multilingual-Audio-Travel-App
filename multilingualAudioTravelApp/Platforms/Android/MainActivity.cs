94901b7b23f3ee44fa9764c5baf62d96a830d508
khang <khangnguyenhhhh1412@gmail.com>
2026-04-22 21:52:06 +0700

    quet qr tai app

--- multilingualAudioTravelApp/Platforms/Android/MainActivity.cs
+++ multilingualAudioTravelApp/Platforms/Android/MainActivity.cs
@@ -1,9 +1,14 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Content; // THÊM DÒNG NÀY ĐỂ HẾT LỖI INTENT
 
 namespace multilingualAudioTravelApp
 {
+    [IntentFilter(new[] { Intent.ActionView },
+    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+    DataScheme = "vktour")]
+
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
